2d6b566bb0f674f4821d44beb2ee774753e6952b
xxandeer <sjostenalex@gmail.com>
2024-01-18 18:12:19 +0100

    Ensure Git installation during setup if not already installed.

---
+++ install/install_git.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Installing Git for Windows...
+
+:: Specify the download URL for the Git installer
+set "gitInstallerURL=https://github.com/git-for-windows/git/releases/download/v2.35.0.windows.1/Git-2.35.0-64-bit.exe"
+
+:: Specify the path to save the installer
+set "installerPath=%TEMP%\GitInstaller.exe"
+
+:: Download the Git installer
+curl -L -o "%installerPath%" "%gitInstallerURL%"
+
+:: Install Git silently
+start /wait "" "%installerPath%" /VERYSILENT /NORESTART /CLOSEAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+
+:: Delete the installer
+del "%installerPath%"
+
+echo Git installation completed.
