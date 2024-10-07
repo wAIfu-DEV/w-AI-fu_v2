11945aebb2f81eb39b8e90805c15c2d305f0dfd9
AiV <155333657+isseluxx@users.noreply.github.com>
2024-10-07 09:47:26 -0700

    Add files via upload

--- source/app/config/config.ts
+++ source/app/config/config.ts
@@ -386,7 +386,7 @@ export class Config {
             hint: "Model used when selecting openai as LLM provider.",
             reload: false,
             type: "select",
-            options: ["gpt-3.5-turbo", "gpt-4"],
+            options: ["gpt-3.5-turbo", "gpt-4", "gpt-4o-mini", "llama-3.1-70b-versatile"],
             default: "gpt-3.5-turbo",
             value: "gpt-3.5-turbo",
         }),
